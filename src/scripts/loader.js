21d4e43ffafce593e1b83b0607d46328abb6c2f5
Kiahn Johnston-Lepore <kiahnjl@gmail.com>
2019-01-24 18:10:58 +1100

    Css prefixes

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
     document.body.classList.remove('page--loading');
     document.body.classList.add('page--loaded-enter');
+    
     setTimeout(function() {
         document.body.classList.remove('page--loaded-enter');
         document.body.classList.add('page--loaded-active');
