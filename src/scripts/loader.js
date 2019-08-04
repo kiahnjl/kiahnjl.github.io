0e3d46e3076ba8402e3afadf5f8f3eb2bbb9707f
Kiahn Johnston-Lepore <kiahn@qwilr.com>
2019-08-05 08:59:55 +1000

    Basic mag

--- src/scripts/loader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    document.body.classList.remove('page--loading');
-    document.body.classList.add('page--loaded-enter');
-    
-    setTimeout(function() {
-        document.body.classList.remove('page--loaded-enter');
-        document.body.classList.add('page--loaded-active');
-    }, 500);
-});
